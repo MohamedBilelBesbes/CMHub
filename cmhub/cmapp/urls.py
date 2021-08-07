74a638ad5a32ff1133c794b71eb6e9d1b9b0fbea
ChaimaBeldi <chaima.beldi@supcom.tn>
2021-08-07 18:39:11 +0200

    Post CRUD added

--- cmhub/cmapp/urls.py
+++ cmhub/cmapp/urls.py
@@ -12,4 +12,9 @@ urlpatterns = [
     path('logout/', auth_view.LogoutView.as_view(template_name='cmapp/logout.html'), name="logout"),
     path('edit_profile/', views.edit_profile,name='edit_profile'),
     url(r'^delete_user/(?P<pk>.*)', views.delete_user, name='delete_user'),
+    url(r'create_post/(?P<owner>.*)', views.create_post,name='create_post'),
+    path('display_posts/', views.display_posts,name='display_posts'),
+    url(r'^delete_post/(?P<pk>.*)', views.delete_post, name='delete_post'),
+    url(r'^edit_post/(?P<idpost>.*)', views.edit_post, name='edit_post'),
+    url(r'^display_post/(?P<pk>.*)', views.display_post, name='display_post'),
 ]
--- cmhub/cmapp/views.py
+++ cmhub/cmapp/views.py
@@ -4,12 +4,11 @@ from .forms import UserRegisterForm, EditProfileForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from .models import Post
 
-
+#Users
 def index(request):
     return render(request, 'cmapp/index.html')
-
-
 
 def register(request):
     if request.method == "POST":
@@ -28,7 +27,7 @@ def register(request):
 @login_required()
 def profile(request):
     return render(request, 'cmapp/profile.html')
-
+@login_required()
 def edit_profile(request):
     if request.method == 'POST':
         form = EditProfileForm(request.POST, instance=request.user)
@@ -40,9 +39,109 @@ def edit_profile(request):
         form = EditProfileForm(instance=request.user)
         args = {'form': form}
         return render(request, 'cmapp/edit_profile.html', args)
-
+@login_required()
 def delete_user(request,pk):
     user = User.objects.filter(username=pk)
     user.delete()
     return redirect('index')
 
+#Posts
+def create_post(request, owner):
+    # ...
+    if request.method == 'POST':
+       
+        Numberoffollowers = int(request.POST.get('numberoffollowers'))
+        Numberoffollowing = int(request.POST.get('numberoffollowing'))
+        Numberoftweets = int(request.POST.get('numberoftweets'))
+      
+        Content = str(request.POST.get('content'))
+        Picture = request.POST.get('picture')
+        video = request.POST.get('video')
+        
+        Owner=User.objects.get(pk=owner).username
+        if video == "no":
+            video=False
+        elif video == "yes" :
+            video=True
+        
+        if Picture == "no":
+            Picture=False
+        elif Picture == "yes" :
+            Picture=True
+        post = Post.objects.create(
+              numberoffollowers = Numberoffollowers,
+              numberoffollowing = Numberoffollowing,
+              numberoftweets = Numberoftweets,
+              content = Content,
+              picture = Picture,
+              video = video,
+              owner = Owner,
+
+            )
+        return redirect('index')
+
+    return render(request, 'posts/createposts.html')
+@login_required()
+def display_posts(request):
+        posts = Post.objects.all()
+        args = {'posts': posts}
+        return render(request, 'posts/display_posts.html', args)
+@login_required()
+def delete_post(request,pk):
+    post = Post.objects.filter(pk=pk)
+    post.delete()
+    return redirect('index')
+@login_required()
+def edit_post(request,idpost):
+    if request.method == 'POST':
+        post = Post.objects.filter(pk=idpost)
+        Numberoffollowers = int(request.POST.get('numberoffollowers'))
+        Numberoffollowing = int(request.POST.get('numberoffollowing'))
+        Numberoftweets = int(request.POST.get('numberoftweets'))
+      
+        Content = str(request.POST.get('content'))
+        Picture = request.POST.get('picture')
+        video = request.POST.get('video')
+        
+        #Owner=User.objects.get(pk=owner).username
+        if video == "no":
+            video=False
+        elif video == "yes" :
+            video=True
+        
+        if Picture == "no":
+            Picture=False
+        elif Picture == "yes" :
+            Picture=True
+        post.update(
+              numberoffollowers = Numberoffollowers,
+              numberoffollowing = Numberoffollowing,
+              numberoftweets = Numberoftweets,
+              content = Content,
+              picture = Picture,
+              video = video,
+              
+
+            )
+        return redirect('index')
+    else :
+        post = Post.objects.get(pk=idpost)
+        Picture = post.picture
+        Video = post.video
+        if Picture == False:
+            Picture = "no"
+        elif Picture == True :
+            Picture= "yes"
+        if Video == False:
+            Video = "no"
+        elif Video == True :
+            Video= "yes"
+        args = {'post' : post, 'picture': Picture, 'video': Video}
+        
+
+    return render(request, 'posts/edit_post.html', args)
+@login_required()
+def display_post(request, pk):
+        post = Post.objects.get(pk=pk)
+        args = {'post': post}
+        return render(request, 'posts/display_post.html', args)
